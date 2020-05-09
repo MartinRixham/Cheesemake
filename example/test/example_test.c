6291c82199d55568a07e4225f2698cd060286d9e
Martin Rixham <martin.rixham@gmail.com>
2020-05-09 18:04:08 +0100

    Run tests.

--- example/test/example_test.c
+++ example/test/example_test.c
@@ -6,8 +6,33 @@ START_TEST (test_name)
 }
 END_TEST
 
+Suite *money_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Test");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_name);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
 
 int main(void)
 {
-	return 0;
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = money_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return number_failed;
 }
